bd10cb32fe93ce165073ad3abdc62e36681b7d4d
hzhwcmhf <hzhwcmhf@gmail.com>
2020-04-02 17:25:36 +0800

    fix test

--- utils/random.py
+++ utils/random.py
@@ -1,9 +1,9 @@
-from scipy.stats import truncnorm
 from operator import mul
 import numpy as np
 from functools import reduce
 
 def truncated_normal(*size, threshold=2):
+	from scipy.stats import truncnorm
 	all_size = reduce(mul, size, 1)
 	values = truncnorm.rvs(-threshold, threshold, size=all_size)
 	return np.reshape(values, size)
